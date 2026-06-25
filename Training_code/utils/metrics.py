0583c92598d86163cf0fffe0a086f380f6bb4ec5
Corngit <erichym0407@gmail.com>
2026-06-25 17:04:24 +0800

    add folder

---
+++ Training_code/utils/metrics.py
@@ -0,0 +1,78 @@
+"""
+Author: Smeet Shah
+Copyright (c) 2020 Smeet Shah
+File part of 'deep_avsr' GitHub repository available at -
+https://github.com/smeetrs/deep_avsr
+"""
+
+import torch
+import numpy as np
+import editdistance
+
+
+
+def compute_cer(predictionBatch, targetBatch, predictionLenBatch, targetLenBatch):
+
+    """
+    Function to compute the Character Error Rate using the Predicted character indices and the Target character
+    indices over a batch.
+    CER is computed by dividing the total number of character edits (computed using the editdistance package)
+    with the total number of characters (total => over all the samples in a batch).
+    The <EOS> token at the end is excluded before computing the CER.
+    """
+
+    targetBatch = targetBatch.cpu()
+    targetLenBatch = targetLenBatch.cpu()
+
+    preds = list(torch.split(predictionBatch, predictionLenBatch.tolist()))
+    trgts = list(torch.split(targetBatch, targetLenBatch.tolist()))
+    totalEdits = 0
+    totalChars = 0
+
+    for n in range(len(preds)):
+        pred = preds[n].numpy()[:-1]
+        trgt = trgts[n].numpy()[:-1]
+        numEdits = editdistance.eval(pred, trgt)
+        totalEdits = totalEdits + numEdits
+        totalChars = totalChars + len(trgt)
+    if totalChars == 0:
+        return 1.0
+    return totalEdits/totalChars
+
+
+
+def compute_wer(predictionBatch, targetBatch, predictionLenBatch, targetLenBatch, spaceIx):
+
+    """
+    Function to compute the Word Error Rate using the Predicted character indices and the Target character
+    indices over a batch. The words are obtained by splitting the output at spaces.
+    WER is computed by dividing the total number of word edits (computed using the editdistance package)
+    with the total number of words (total => over all the samples in a batch).
+    The <EOS> token at the end is excluded before computing the WER. Words with only a space are removed as well.
+    """
+
+    targetBatch = targetBatch.cpu()
+    targetLenBatch = targetLenBatch.cpu()
+
+    preds = list(torch.split(predictionBatch, predictionLenBatch.tolist()))
+    trgts = list(torch.split(targetBatch, targetLenBatch.tolist()))
+    totalEdits = 0
+    totalWords = 0
+
+    for n in range(len(preds)):
+        pred = preds[n].numpy()[:-1]
+        trgt = trgts[n].numpy()[:-1]
+
+        predWords = np.split(pred, np.where(pred == spaceIx)[0])
+        predWords = [predWords[0].tobytes()] + [predWords[i][1:].tobytes() for i in range(1, len(predWords)) if len(predWords[i][1:]) != 0]
+
+        trgtWords = np.split(trgt, np.where(trgt == spaceIx)[0])
+        trgtWords = [trgtWords[0].tobytes()] + [trgtWords[i][1:].tobytes() for i in range(1, len(trgtWords))]
+
+        numEdits = editdistance.eval(predWords, trgtWords)
+        totalEdits = totalEdits + numEdits
+        totalWords = totalWords + len(trgtWords)
+
+    if totalWords == 0:
+        return 1.0
+    return totalEdits/totalWords
